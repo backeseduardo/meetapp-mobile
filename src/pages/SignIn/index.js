1421bd6fa6ebfc05be49d5beba2fbcb9c8a5b215
Eduardo Backes <e.backes2011@gmail.com>
2019-07-28 16:40:32 -0300

    initial commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Input placeholder="Call" style={{ marginTop: 30 }} />
+      <Button>Entrar</Button>
+    </Background>
+  );
+}
