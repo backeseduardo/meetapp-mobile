49196a2f2bf541c179759e0dfa070a71ba2e1c7e
Eduardo Backes <e.backes2011@gmail.com>
2019-07-28 17:40:21 -0300

    SignIn and SignUp pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,16 +1,54 @@
 import React from 'react';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
 
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-export default function SignIn() {
+import logo from '~/assets/logo.png';
+
+export default function SignIn({ navigation }) {
   return (
     <Background>
-      <Input placeholder="Call" style={{ marginTop: 30 }} />
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+          />
+
+          <SubmitButton>Entrar</SubmitButton>
+
+          <SignLink onPress={() => navigation.navigate('SignUp')}>
+            <SignLinkText>Criar conta grátis</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
     </Background>
   );
 }
+
+SignIn.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,14 +1,61 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-export default function SignUp() {
+import logo from '~/assets/logo.png';
+
+export default function SignUp({ navigation }) {
   return (
     <Background>
-      <Text>SignUp</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+          />
+
+          <SubmitButton>Criar conta</SubmitButton>
+
+          <SignLink onPress={() => navigation.navigate('SignIn')}>
+            <SignLinkText>Já tenho login</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
     </Background>
   );
 }
+
+SignUp.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
